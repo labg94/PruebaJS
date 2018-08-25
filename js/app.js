fd789a86c695f197a0b5e54eef6ba602842248d4
Luis Benavides <Luisalejobg@gmail.com>
2018-08-25 14:31:41 -0300

    funciones de punto, on, signo listas

--- js/app.js
+++ js/app.js
@@ -1,41 +1,80 @@
+var pantalla = document.getElementById("display")
 
-var pantalla= document.getElementById("display")
-
-function animacionBoton(obj){
-obj.onmousedown = onClickDown(obj);
-obj.onmouseup= onClickUp(obj);
+function animacionBoton(obj) {
+  obj.onmousedown = onClickDown(obj);
+  obj.onmouseup = onClickUp(obj);
 }
 
-var n1= document.getElementById("1");
-var n2= document.getElementById("2");
-var n3= document.getElementById("3");
-var n4= document.getElementById("4");
-var n5= document.getElementById("5");
-var n6= document.getElementById("6");
-var n7= document.getElementById("7");
-var n8= document.getElementById("8");
-var n9= document.getElementById("9");
+var n1 = document.getElementById("1");
+var n2 = document.getElementById("2");
+var n3 = document.getElementById("3");
+var n4 = document.getElementById("4");
+var n5 = document.getElementById("5");
+var n6 = document.getElementById("6");
+var n7 = document.getElementById("7");
+var n8 = document.getElementById("8");
+var n9 = document.getElementById("9");
+var n0 = document.getElementById("0");
+var on = document.getElementById("on");
+var punto = document.getElementById("punto");
+var igual = document.getElementById("igual");
+var signo = document.getElementById("sign");
 
-
-n1.onmousedown= function(){onClickDown(this)};
-n1.onmouseup= function(){onClickUp(this)};
-
-n1.onclick= function(){escribir(this)}
-
-
-function onClickDown(obj){
-  obj.style.width = "20%";
+n1.onclick = function() {
+  escribir(this)
 }
-function onClickUp(obj){
-  obj.style.width = "22%";
+n2.onclick = function() {
+  escribir(this)
+}
+n3.onclick = function() {
+  escribir(this)
+}
+n4.onclick = function() {
+  escribir(this)
+}
+n5.onclick = function() {
+  escribir(this)
+}
+n6.onclick = function() {
+  escribir(this)
+}
+n7.onclick = function() {
+  escribir(this)
+}
+n8.onclick = function() {
+  escribir(this)
+}
+n9.onclick = function() {
+  escribir(this)
+}
+n0.onclick = function() {
+  escribir(this)
 }
 
-function escribir(obj){
-  var num=  obj.getAttribute("id");
+on.onclick = function() {
+  pantalla.innerHTML = 0
+}
 
-  if(pantalla.innerHTML==0){
-      pantalla.innerHTML= num;
+
+punto.onclick = function() {
+  var existe = pantalla.innerHTML.indexOf(".");
+  if (existe == -1) {
+    pantalla.innerHTML += "."
+  }
+}
+
+signo.onclick = function() {
+  pantalla.innerHTML = pantalla.innerHTML * -1
+}
+
+function escribir(obj) {
+  var num = obj.getAttribute("id");
+
+  if (pantalla.innerHTML == "0") {
+    pantalla.innerHTML = num;
+  } else if(pantalla.innerHTML.length<8) {
+    pantalla.innerHTML += num;
   }else{
-      pantalla.innerHTML+= num;
+    
   }
 }
