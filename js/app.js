595b75f2332a1eb2f2c2105cb01d459335cc13f7
Luis Benavides <Luisalejobg@gmail.com>
2018-08-25 16:02:29 -0300

    suma y resta lista

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 var pantalla = document.getElementById("display")
-
-function animacionBoton(obj) {
-  obj.onmousedown = onClickDown(obj);
-  obj.onmouseup = onClickUp(obj);
-}
-
+var valorInicial;
 var n1 = document.getElementById("1");
 var n2 = document.getElementById("2");
 var n3 = document.getElementById("3");
@@ -19,9 +14,13 @@ var on = document.getElementById("on");
 var punto = document.getElementById("punto");
 var igual = document.getElementById("igual");
 var signo = document.getElementById("sign");
+var suma = document.getElementById("mas");
+var resta = document.getElementById("menos");
+var multiplicacion = document.getElementById("por");
+var division = document.getElementById("dividido");
 
 n1.onclick = function() {
-  escribir(this)
+  escribir(this);
 }
 n2.onclick = function() {
   escribir(this)
@@ -53,8 +52,31 @@ n0.onclick = function() {
 
 on.onclick = function() {
   pantalla.innerHTML = 0
+  valorTotal = 0;
 }
 
+suma.onclick = function() {
+  resultado()
+  sumar()
+}
+
+resta.onclick = function() {
+  resultado()
+  restar()
+}
+multiplicacion.onclick = function() {
+  resultado()
+  multiplicar()
+}
+division.onclick = function() {
+  resultado()
+  dividir()
+}
+
+igual.onclick = function() {
+  resultado()
+  mostrarResultado()
+}
 
 punto.onclick = function() {
   var existe = pantalla.innerHTML.indexOf(".");
@@ -69,12 +91,112 @@ signo.onclick = function() {
 
 function escribir(obj) {
   var num = obj.getAttribute("id");
-
   if (pantalla.innerHTML == "0") {
     pantalla.innerHTML = num;
-  } else if(pantalla.innerHTML.length<8) {
+    valorInicial = num
+  } else if (pantalla.innerHTML.length < 8) {
     pantalla.innerHTML += num;
-  }else{
-    
+    valorInicial += num;
+  } else {
+    valorInicial += num
   }
+  return valorInicial;
+}
+
+function resultado() {
+  if (suma) {
+    sumar();
+    suma = false;
+  }
+  if (resta) {
+    restar();
+    resta = false;
+  }
+  if (multi) {
+    multiplicar();
+    multi = false;
+  }
+  if (divi) {
+    dividir();
+    divi = false;
+  }
+
+  if (valorTotal == 0) {
+    valorTotal = valorInicial;
+  }
+
+
+}
+
+function mostrarResultado() {
+  pantalla.innerHTML = valorTotal;
+}
+
+
+var valorTotal = 0;
+var suma = false;
+var resta = false;
+var multi = false;
+var divi = false;
+
+function limpiar() {
+  valorInicial = "";
+  pantalla.innerHTML = "";
+}
+
+
+function sumar() {
+  resta = false;
+  multi = false;
+  divi = false;
+  suma = true;
+  valorTotal += Number(valorInicial);
+  limpiar();
+}
+
+function restar() {
+  suma = false;
+  multi = false;
+  divi = false;
+  resta = true;
+  if (valorTotal == 0) {
+    valorTotal = valorInicial;
+  } else {
+    valorTotal -= Number(valorInicial);
+  }
+  limpiar();
+
+}
+
+function multiplicar() {
+  resta = false;
+  suma = false;
+  divi = false;
+  multi = true;
+  if (valorTotal == 0) {
+    valorTotal = valorInicial;
+  } else {
+    valorTotal *= Number(valorInicial);
+  }
+  limpiar();
+
+}
+
+function dividir() {
+  resta = false;
+  multi = false;
+  suma = false;
+  divi = true;
+  console.log("@antes de todo: " +valorTotal)
+  if (valorTotal == 0) {
+    console.log("@deberia ser 0: "+valorTotal)
+    valorTotal = valorInicial;
+    console.log("@deberia ser ahora el valor inicial:"+valorTotal+"\nvalor inicial: "+ valorInicial)
+  } else {
+    console.log("@Empezo la division"+ valorTotal+"\nentre: "+valorInicial);
+    valorTotal /= Number(valorInicial);
+    console.log("@valorTotal despues de la division: "+ valorTotal+"\nvalor inicial luego de la division: " +valorInicial);
+  }
+  limpiar();
+
 }
